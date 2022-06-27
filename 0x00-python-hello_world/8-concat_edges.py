48ee3d13b896c27c3900b865f29a0303655e4d17
Zayyad Musa <alias.co>
2022-06-27 16:52:58 +0100

    amother

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
-# object-oriented programming with Python
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-
-concat = str[38:66] + ' ' + str[106:110] + ' ' + str[:6]
-
+    language that combines remarkable power with very clear syntax"
+concat = str[38:66] + str[-23:-17] + str[0:6]
 print(concat)
