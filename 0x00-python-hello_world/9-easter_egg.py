660bf0542d566dd7f6d50eb30a7f605a8d1da607
Zayyad Musa <alias.co>
2022-06-28 11:10:34 +0100

    ads

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 import this
+
+a = 12
+if a < 2:
+    print("Holberton")
+elif a % 2 == 0:
+    print("C is fun")
+else:
+    print("School")
+for i in range(2,4):
+    print(i, end=" ")
