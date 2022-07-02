b472fef936545356f1818ab66ce9ef9f1335fea3
Zayyad Musa <alias.co>
2022-07-02 20:12:59 +0100

    newerr

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-from typing import List
 
-lst: List = [1,2,3,4,5]
-
-def print_list_integer(lst: List) -> List:
-    for i in lst:
-        print(str(i))
-
+def print_list_integer(my_list: list = []) -> list:
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-from typing import List
-
-def element_at(my_list: List, idx: int) -> int:
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
+def element_at(my_list: list, idx: int):
+    if idx < 0 or idx > len(my_list) - 1:
+        return "None"
     else:
         return my_list[idx]
