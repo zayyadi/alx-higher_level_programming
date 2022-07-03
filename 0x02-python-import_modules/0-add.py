b63086aa63252f737ac5dc2cef354620d761b089
Zayyad Musa <alias.co>
2022-07-03 13:52:14 +0100

    newer cyy

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from typing import List
 
-def list_print(lst: List) -> List:
-    for i in lst:
-        print(str(i))
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
