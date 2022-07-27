f44ba883f1151d87090dbfadb5fea10dc386a34d
Zayyad Musa <alias.co>
2022-07-27 05:34:34 +0100

    mann

---
+++ 0x07-python-test_driven_development/0_add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def add_integer(a: int, b: int) -> int:
+    """Add two integers together."""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
